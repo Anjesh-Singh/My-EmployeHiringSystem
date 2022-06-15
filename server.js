5627a032664c550309d5183269b8d38718d17a5b
Anjesh singh <anjeshsingh8484@gmail.com>
2022-06-15 10:20:28 +0530

    install dotenv in project

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');                //body-parser help to get access of data which send by user in body
 const app = express();
+require('dotenv').config;                                // to access env file
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {                    // process.env.PORT  using process we select .env file and then we select PORT
     console.log("Server Started")
 })
