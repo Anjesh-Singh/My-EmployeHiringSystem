87bb013e3070014a86b6691657b467e53c2ce0c4
Anjesh singh <anjeshsingh8484@gmail.com>
2022-06-19 14:59:09 +0530

    jwt token added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,12 @@
 const authService = require('../services/auth.services');
 
-
+//------------------------------------------------------------------------------------------------------
 const serverError = {
     message: "something wents wrong",
     success: false,
     data: {}
 }
+//------------------------------------------------------------------------------------------------------
 
 const signup = async (req, res) => {
     const response = await authService.signup(req.body)
@@ -26,7 +27,42 @@ const signup = async (req, res) => {
         data: response
     })
 }
+//------------------------------------------------------------------------------------------------------
 
+const signin = async (req, res) => {
+    const user = await authService.getUserByEmail(req.body.email);
+    if(!user){
+        return res.status(404).json({
+            message: "No user found by email",
+            success: false,
+            data: {}
+        })
+    }
+    if(user.error){
+        console.log(user.error);
+        return res.status(500).json(serverError);
+
+    }
+    if(!authService.checkPassword(req.body.password, user.password)){   //If we remove ! from this line it will accept any wrong password to signin
+        return res.status(400).json({                                   // because checkpassword is returing value as true/false
+            message: "Wrong password",
+            success: false,
+            data: {}
+        })
+    }
+    const token = authService.creatToken({id: user.id, email: user.email});
+    if(!token){
+        return res.status(500).json(serverError)
+    }
+    return res.status(200).json({
+        message: "Successfully signed in",
+        success: true,
+        data: token
+    })
+}
+
+//------------------------------------------------------------------------------------------------------
 module.exports = {
-    signup
+    signup,
+    signin
 }
--- services/auth.services.js
+++ services/auth.services.js
@@ -1,4 +1,8 @@
 const { User } = require('../models/index');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+
+//------------------------------------------------------------------------------------------------------
 
 const signup = async (data) => {
     try {
@@ -7,8 +11,8 @@ const signup = async (data) => {
     }
     catch (err) {
         console.log(err.errors)
-        if(err.name == 'SequelizeUniqueConstraintError' || err.name == 'SequelizeValidationError'){
-            return{
+        if (err.name == 'SequelizeUniqueConstraintError' || err.name == 'SequelizeValidationError') {
+            return {
                 error: err.message,
                 details: err.errors[0].message
             }
@@ -16,7 +20,48 @@ const signup = async (data) => {
     }
 }
 
+//------------------------------------------------------------------------------------------------------
+const getUserByEmail = async (userEmail) => {
+    try{
+        const user = await User.findOne({           //when findOne will find user in database it will return all data about that email
+            where:{                                 //And if not find any value then it will return null
+                email: userEmail
+            }
+        });
+        return user;
+    }
+    catch(err){
+        console.log(err)
+        return {
+            error : err.message
+        }
+    }
+    
+}
+//------------------------------------------------------------------------------------------------------
+const checkPassword = (userPass, encryptPass) => {
+    return bcrypt.compareSync(userPass, encryptPass);   //checkPassword will return value in true/false
+    
+}
+//------------------------------------------------------------------------------------------------------
+
+const creatToken = (user) => {
+    try{
+        return jwt.sign(user, 'relevel', {
+            expiresIn: '2 days'
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
+} 
+
+
+
 
 module.exports = {
-    signup
+    signup,
+    getUserByEmail,
+    checkPassword,
+    creatToken
 }
