fa95aed6e8b7747dcd3553f8a84f0582828cbb2b
Anjesh singh <anjeshsingh8484@gmail.com>
2022-06-21 22:38:38 +0530

    Added role model and associated with user

--- models/user.js
+++ models/user.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsToMany(models.Role,{
+        through: 'User_Roles',
+        foreignKey: 'userid',
+        otherKey: 'roleid'
+      });
     }
   }
   User.init({
